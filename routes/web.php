9de1229d6138c8285a05fcadbd26ff0ec6a4542d
SpaghettiWolf21 <142078415+SpaghettiWolf21@users.noreply.github.com>
2024-05-27 13:21:33 +0800

    Latest changes

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,7 @@
 */
 
 /*
-* PHP Attendance Management System
-* Email: heyalexluna@gmail.com
-* Version: 1.1
-* Author: Alexis Luna
-* Copyright 2019 Alexis Luna
-* Website: https://github.com/mralexisluna/php-attendance-management-system
+* Racing R Attendance Monitoring System
 */
 
 Route::group(['middleware' => 'auth'], function () {
